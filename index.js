a96eca1f9d0038b1974440b75757ae097effc2c4
Christine Jeon <csjeon28@gmail.com>
2021-05-03 12:14:49 -0400

    commit tree cc0399e48d39f3be8132a5159c149f67c77c0646

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+  static titleize(string) {
+    const sentence = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = string.split(' ')
+    let capitalizedSentence = [];
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0) {
+        capitalizedSentence.push(this.capitalize(words[i]))
+      } else {
+        if (sentence.includes(words[i])) {
+          capitalizedSentence.push(words[i])
+        } else {
+          capitalizedSentence.push(this.capitalize(words[i]))
+        }
+      }
+    }
+    return capitalizedSentence.join(' ');
+  }
 }
